aec3cb3383da3050d60a46cf3cf08c4630702b67
Karen Eloisa Flores <kareneloisa_f@yahoo.com>
2019-10-25 13:16:33 -0600

    servicios actualizado

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -3,58 +3,91 @@
 namespace App\Http\Controllers;
 
 use App\Servicio;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ServiciosController extends Controller
 {
     public function servicio(Request $request)
     {
+        $servicios = new Servicio();
+        if (($request->foto) == null) {
+            $servicios->nombre = $request->input('nombre');
+            $servicios->descripcion = $request->input('descripcion');
 
-            /*$file = $request->file('imagen');
-            $name = $file->getClientOriginalName();
-            $file->move(public_path().'/imagenes/',$name);*/
+            $servicios->save();
+
+            return response()->json(["mensaje" => 'creado1 correctamente'])
+                ->header('Content_Type', 'application/json')->header('X-Requested-With', 'XMLHttpRequest');
+
+        } else {
+            $exploded = explode(',', $request->foto);
+            $decode = base64_decode($exploded[0]);
+            if (str_contains($exploded[0], 'jpeg'))
+                $extension = 'jpg';
+            else
+                $extension = 'png';
+            $imagen = Carbon::now()->toDateString() ."_".$request->input("nombre"). '_imagen.' . $extension;
+            $path = public_path()."/imagen/".$imagen;
+            file_put_contents($path, $decode);
+
+            $servicios->nombre = $request->input('nombre');
+            $servicios->descripcion = $request->input('descripcion');
+            $servicios->foto = $imagen;
+            $servicios->save();
 
 
-            $file = $request->all();
-            if ($archivo = $request ->file('imagen')) {
-                $nombre = $archivo->getClientOriginalName();
-                $archivo->move(public_path() . '/imagen/', $nombre);
-            $entrada['imagen']=$nombre;
-            }
-            Servicio::create($file);
+            return response()->json(["mensaje"=> 'creado2 correctamente'])
+                ->header('Content_Type', 'application/json')->header('X-Requested-With', 'XMLHttpRequest');
 
-            }
+        }
+    }
+    /*$file = $request->file('imagen');
+    $name = $file->getClientOriginalName();
+    $file->move(public_path().'/imagenes/',$name);
+
+//si no funciona arriba dejar este
+ /*  $file = $request->all();
+    if ($archivo = $request ->file('imagen')) {
+        $nombre = $archivo->getClientOriginalExtension();
+        $archivo->move(public_path() . '/imagen/', $nombre);
+    $entrada['imagen']=$nombre;
+    }
+    Servicio::create($file);
+
+    }*/
     //Jennifer
-    public function Recuperar(){
+    public function Recuperar()
+    {
         $servicios = Servicio::all();
         return response()->json([
-            'servicio'=> $servicios]);
+            'servicio' => $servicios]);
 
     }
 
 //
 
 
-        /*$this->validate($request, [
+    /*$this->validate($request, [
 
-            'nombre' => 'required|string',
-            'descripcion' => 'required|string',
-            'imagen' => 'required',
+        'nombre' => 'required|string',
+        'descripcion' => 'required|string',
+        'imagen' => 'required',
 
-        ]);
+    ]);
 
 
 
-        $Servicios = new Servicio([
+    $Servicios = new Servicio([
 
-            'nombre' => $request->Nombre,
-            'descripcion' => $request->Descripcion,
-             $request->Imagen = $name,
+        'nombre' => $request->Nombre,
+        'descripcion' => $request->Descripcion,
+         $request->Imagen = $name,
 
-        ]);
+    ]);
 
-        $Servicios->save();
-        return response()->json([
-            'Exito'], 201);
-    }*/
+    $Servicios->save();
+    return response()->json([
+        'Exito'], 201);
+}*/
 }
