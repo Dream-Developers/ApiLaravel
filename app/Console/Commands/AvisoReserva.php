f4ca57e38d6d54512d994a902d052b12901045ef
MariIza14 <55727313+MariIza14@users.noreply.github.com>
2020-03-08 02:17:28 -0600

    creacion de comando para la notificacion proxima fecha un dia antes

--- app/Console/Commands/AvisoReserva.php
+++ app/Console/Commands/AvisoReserva.php
@@ -43,7 +43,8 @@ class AvisoReserva extends Command
     public function handle()
     {
 
-        $citas = Cita::where('FechaFumigacion', now()->toDateString())->get();
+        $citas = Cita::where('FechaProxima', now()->toDateString())->get();
+        /** Notifica al usuario administrador que tiene una cita por realizar en su fecha proxima el mismo dia y una hora antes*/
         if ($citas !== null) {
             if ($citas->count() > 0) {
 
@@ -71,12 +72,48 @@ class AvisoReserva extends Command
 
                         );
 
-                        $user->notify(new FirebaseNotification($data,
-                                "Citas Pendientes", $data));
+                        $user->notify(new FirebaseNotification("Tienes una cita pendiente a llevar a cabo dentro de 1 hora del usuario con nombre: ' ". $item->Nombre . "''",
+                                "Citas Pendientes", "Tienes una cita pendiente a llevar a cabo dentro de 1 hora del usuario con nombre: ' ". $item->Nombre . "''"));
                     }
                 }
 
             }
+        }
+
+
+        $citaUnDiaAntes = Cita::all();
+
+        if($citaUnDiaAntes!==null){
+            if($citaUnDiaAntes->count()){
+                foreach ($citaUnDiaAntes as $item){
+                    $horaCita = $item->FechaProxima . "" . $item->Hora;
+                    try {
+                        $Hora = new \DateTime($horaCita);
+                    } catch (\Exception $e) {
+                    }
+
+                    $Hora->modify("-1440 minutes");
+
+                    $Hora->format("Y-m-d H:i");
+                    $carbonHora = Carbon::createFromTimestamp($Hora->getTimestamp());
+
+
+                    if ($carbonHora->between(now()->subMinute(1), now()->addMinute(1))) {
+                        $user = User::where("rol_id", 1)->first();
+
+                        $data = array(
+                            "id_cita" => $item->id,
+                            "body" => "Tienes una cita pendiente a llevar a cabo mañana del usuario con nombre: ' ". $item->Nombre . "''"
+                        , "click_action"=>"Detalle_Cita"
+
+                        );
+
+                        $user->notify(new FirebaseNotification( "Tienes una cita pendiente a llevar a cabo mañana del usuario con nombre: ' ". $item->Nombre . "''",
+                            "Citas Pendientes", "Tienes una cita pendiente a llevar a cabo mañana del usuario con nombre: ' ". $item->Nombre . "''"));
+                    }
+                }
+            }
+
         }
     }
 }
